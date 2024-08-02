61e06d33ab4a2e9439f2f8a78c142b1d67b98be4
emreinan <emreinannn@gmail.com>
2024-08-02 12:54:13 +0300

    Repositories implemented

---
+++ src/corePackages/Core.Application/Services/Repositories/IUserRepository.cs
@@ -0,0 +1,35 @@
+﻿using Core.Persistence.Repositories;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.Services.Repositories;
+
+public interface IUserRepository : IRepository<User, Guid> , IAsyncRepository<User, Guid>
+{
+}
+
+public interface IMessageRepository : IRepository<Message, Guid> , IAsyncRepository<Message, Guid>
+{
+}
+
+public interface IMessageUserStateRepository : IRepository<MessageUserState, Guid> , IAsyncRepository<MessageUserState, Guid>
+{
+}
+
+public interface IRefreshTokenRepository : IRepository<RefreshToken, Guid> , IAsyncRepository<RefreshToken, Guid>
+{
+}
+
+public interface IChatRepository : IRepository<Chat, Guid> , IAsyncRepository<Chat, Guid>
+{
+}
+
+public interface IChatUserRepository : IRepository<ChatUser, Guid> , IAsyncRepository<ChatUser, Guid>
+{
+}
+
+
---
+++ src/messagingApp/Persistence/Repositories/ChatRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.Application.Services.Repositories;
+using Core.Persistence.Repositories;
+using Domain.Entities;
+using Persistence.Contexts;
+
+namespace Persistence.Repositories;
+
+public class ChatRepository : EfRepositoryBase<Chat, Guid, AppDbContext>, IChatRepository
+{
+    public ChatRepository(AppDbContext context) : base(context)
+    {
+    }
+}
+
