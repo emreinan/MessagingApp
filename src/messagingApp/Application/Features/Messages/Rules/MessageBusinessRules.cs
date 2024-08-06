4d7395f5baf394ec2372d1d3883757586cb327c8
emreinan <emreinannn@gmail.com>
2024-08-06 10:23:26 +0300

    feat: send message command implemeted

---
+++ src/messagingApp/Application/Features/Messages/Rules/MessageBusinessRules.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Messages.Rules;
+
+public class MessageBusinessRules
+{
+    public void ChatShouldExistWhenSelected(Chat? chat)
+    {
+        if (chat is null)
+            throw new Exception("Chat not found");
+    }
+    public void UserShouldExistWhenSelected(User? user)
+    {
+        if (user is null)
+            throw new Exception("Chat not found");
+    }
+}
---
+++ src/messagingApp/WebAPI/Controllers/MessagesController.cs
@@ -0,0 +1,19 @@
+﻿using Application.Features.Messages.Commands.SendMessage;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController(IMediator mediator) : ControllerBase
+    {
+        [HttpPost("SendMessage")]
+        public async Task<IActionResult> SendMessage([FromBody] SendMessageCommand command)
+        {
+            var response = await mediator.Send(command);
+            return Ok(response);
+        }
+    }
+}
